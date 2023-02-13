013102c25a224f331d039fd0ab34612840b480d3
DevExpressExampleBot <fakeEmail>
2023-02-13 14:25:45 +0400

    Source auto update [skip ci]

--- VB/HowToCustomGroupInterval/MainWindow.xaml.vb
+++ VB/HowToCustomGroupInterval/MainWindow.xaml.vb
@@ -1,43 +1,37 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Data
-Imports System.Data.OleDb
 Imports System.Windows
 Imports DevExpress.Xpf.PivotGrid
 Imports HowToBindToMDB.NwindDataSetTableAdapters
 Imports System
 
 Namespace HowToBindToMDB
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
-		Private salesPersonDataTable As New NwindDataSet.SalesPersonDataTable()
-		Private salesPersonDataAdapter As New SalesPersonTableAdapter()
 
-		Public Sub New()
-			InitializeComponent()
-			pivotGridControl1.DataSource = salesPersonDataTable
-		End Sub
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			salesPersonDataAdapter.Fill(salesPersonDataTable)
-			pivotGridControl1.CollapseAllRows()
-		End Sub
+        Private salesPersonDataTable As NwindDataSet.SalesPersonDataTable = New NwindDataSet.SalesPersonDataTable()
 
-        Private Sub pivotGridControl1_CustomGroupInterval(ByVal sender As Object, _
-                                                          ByVal e As PivotCustomGroupIntervalEventArgs)
-            If (Not Object.ReferenceEquals(e.Field, fieldProductGroup)) Then
-                Return
-            End If
+        Private salesPersonDataAdapter As SalesPersonTableAdapter = New SalesPersonTableAdapter()
+
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.pivotGridControl1.DataSource = salesPersonDataTable
+        End Sub
+
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            salesPersonDataAdapter.Fill(salesPersonDataTable)
+            Me.pivotGridControl1.CollapseAllRows()
+        End Sub
+
+        Private Sub pivotGridControl1_CustomGroupInterval(ByVal sender As Object, ByVal e As PivotCustomGroupIntervalEventArgs)
+            If Not Object.ReferenceEquals(e.Field, Me.fieldProductGroup) Then Return
             Dim productName As String = Convert.ToString(e.Value)
-            If productName.Chars(0) < "F"c Then
+            If productName(0) < "F"c Then
                 e.GroupValue = "A-E"
-            ElseIf productName.Chars(0) > "E"c AndAlso productName.Chars(0) < "T"c Then
+            ElseIf productName(0) > "E"c AndAlso productName(0) < "T"c Then
                 e.GroupValue = "F-S"
-            ElseIf productName.Chars(0) > "S"c Then
+            ElseIf productName(0) > "S"c Then
                 e.GroupValue = "T-Z"
             End If
         End Sub
-	End Class
+    End Class
 End Namespace
